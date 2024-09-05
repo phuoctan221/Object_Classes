bfa3be5bed876a6dd3854bb61d0b11e5ff3731b0
John Dang <151425388+phuoctan221@users.noreply.github.com>
2024-09-05 21:39:34 +0700

    Lecture8_1

---
+++ src/lec8/Processor1.java
@@ -0,0 +1,13 @@
+package overloading;
+
+public class Processor1 {
+    public static void main(String[] args) {
+//        Overloading ovl = new Overloading();
+//        System.out.println(ovl.sum(1,2,3));
+//        System.out.println(ovl.sum(1,3));
+        System.out.println(Overloading.sum(4,33));
+        System.out.println(Overloading.sum(3,4));
+        System.out.println(Overloading.sum(3,4,5));
+    }
+   
+}
